72720c823f2f70da4f80773bcd3ab30230e122aa
DannySuarez <suarezd10@gmail.com>
2019-08-21 16:42:42 -0700

    updated url

--- src/services/ArtistApi.js
+++ src/services/ArtistApi.js
@@ -1,4 +1,4 @@
-export const getArtist = (artist = 'nirvana') => {
+export const getArtist = (artist) => {
   return fetch(`http://musicbrainz.org/ws/2/artist?query=${artist}&fmt=json&limit=25`)
     .then(res => ([res.ok, res.json()]))
     .then(([ok, json]) => {
