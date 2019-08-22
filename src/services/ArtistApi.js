d71e3d144aa8641b3eec6a30fa229a51c91b66a5
DannySuarez <suarezd10@gmail.com>
2019-08-22 15:03:21 -0700

    Add function to fetch releases

--- src/services/ArtistApi.js
+++ src/services/ArtistApi.js
@@ -6,3 +6,12 @@ export const getArtist = (artist) => {
       return json;
     });
 };
+
+export const getReleases = () => {
+  return fetch('http://musicbrainz.org/ws/2/release?artist=8bfac288-ccc5-448d-9573-c33ea2aa5c30&fmt=json')
+    .then(res => ([res.ok, res.json()]))
+    .then(([ok, json]) => {
+      if(!ok) throw 'Cant Fetch';
+      return json;
+    });
+};
