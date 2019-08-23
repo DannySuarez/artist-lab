fabf39251d81d00f52257a0f82f0702b4d7943fc
DannySuarez <suarezd10@gmail.com>
2019-08-23 10:09:58 -0700

    Add Routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,19 @@
 import React from 'react';
+import {  
+  BrowserRouter as Router,
+  Route,
+  Switch } from 'react-router-dom';
+
 import Home from './containers/Home';
+import ArtistView from '../components/containers/ArtistView';
 
 export default function App() {
-  return <Home />;
+  return (
+    <Router >
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route path="/releases/:artistName/:id" component={ArtistView} />
+      </Switch>
+    </Router>
+  );
 }
