de367e987b688a2a0a49bb1222195ecc360a4345
DannySuarez <suarezd10@gmail.com>
2019-08-23 15:48:42 -0700

    Add props to display artist name

--- src/components/artistView/ReleaseItem.js
+++ src/components/artistView/ReleaseItem.js
@@ -1,16 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-function ArtistItem({ release }) {
-  
+function ArtistItem({ release, artistName }) {
   const image = release['cover-art-archive'].front 
     ? `http://coverartarchive.org/release/${release.id}/front-250` 
     : '/src/images/placeholder.jpg';
 
   return (
     <div>
-      <p>Artist: </p>
-      <p>Name: {release.title}</p>
+      <p>Artist: {artistName} </p>
+      <Link to={`/songs/${release.title}/${release.id}`}>
+        <p>Name: {release.title}</p>
+      </Link>
       <img src={image} />
     </div>
   );
@@ -18,6 +20,7 @@ function ArtistItem({ release }) {
 
 ArtistItem.propTypes = {
   release: PropTypes.object.isRequired,
+  artistName: PropTypes.string.isRequired
 };
 
 export default ArtistItem;
