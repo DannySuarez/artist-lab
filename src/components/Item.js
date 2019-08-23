cca41e0eda19a73636227ae36bdf9ec5e247a6fb
DannySuarez <suarezd10@gmail.com>
2019-08-23 10:09:42 -0700

    Add link functionality

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 function Item({ artist }) {
 
   return (
     <div>
-      <p><span>Artist: </span>{artist.name}</p>
+      <Link to={`/releases/${artist.name}/${artist.id}`}>
+        <p><span>Artist: </span>{artist.name}</p>
+      </Link>
       <p><span>Country: </span> {artist.country}</p>
+      <p>{artist.id}</p>
     </div>
   );
 
