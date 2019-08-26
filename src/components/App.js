b57114f54993ebdac1e22e9add1775e0ffc7f7c0
DannySuarez <suarezd10@gmail.com>
2019-08-26 08:55:55 -0700

    Add paths for new components

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,8 @@ import {
 
 import Home from './containers/Home';
 import ArtistView from '../components/containers/ArtistView';
+import SongView from './containers/SongView';
+import Lyrics from './containers/Lyrics';
 
 export default function App() {
   return (
@@ -13,6 +15,8 @@ export default function App() {
       <Switch>
         <Route exact path="/" component={Home}/>
         <Route path="/releases/:artistName/:id" component={ArtistView} />
+        <Route path="/songs/:artistName/:songName/:id" component={SongView} />
+        <Route path="/lyrics/:artistName/:songName/" component={Lyrics} />
       </Switch>
     </Router>
   );
--- src/services/ArtistApi.js
+++ src/services/ArtistApi.js
@@ -1,5 +1,5 @@
-export const getArtist = (artist) => {
-  return fetch(`http://musicbrainz.org/ws/2/artist?query=${artist}&fmt=json&limit=25`)
+export const getArtist = (artist, page = 1) => {
+  return fetch(`http://musicbrainz.org/ws/2/artist?query=${artist}&fmt=json&limit=25&`)
     .then(res => ([res.ok, res.json()]))
     .then(([ok, json]) => {
       if(!ok) throw 'Cant Fetch';
@@ -7,11 +7,30 @@ export const getArtist = (artist) => {
     });
 };
 
-export const getReleases = () => {
-  return fetch('http://musicbrainz.org/ws/2/release?artist=8bfac288-ccc5-448d-9573-c33ea2aa5c30&fmt=json')
+export const getReleases = (id) => {
+  return fetch(`http://musicbrainz.org/ws/2/release?artist=${id}&fmt=json`)
     .then(res => ([res.ok, res.json()]))
     .then(([ok, json]) => {
       if(!ok) throw 'Cant Fetch';
       return json;
     });
+};
+
+export const getSongs = (id) => {
+  return fetch(`http://musicbrainz.org/ws/2/recording?release=${id}&fmt=json`)
+    .then(res => ([res.ok, res.json()]))
+    .then(([ok, json]) => {
+      if(!ok) throw 'Cant fetch songs';
+      return json;
+    });
+};
+
+export const getLyrics = (artist, song) => {
+  return fetch(`https://api.lyrics.ovh/v1/${artist}/${song}`)
+    .then(res => ([res.ok, res.json()]))
+    .then(([ok, json]) => {
+      if(!ok) throw 'Cant fetch songs';
+      return json;
+    });
+
 };
